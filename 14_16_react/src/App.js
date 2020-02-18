483c1c6b26d498fdb83bd9725ae0562789053034
creativegeniusod <creativegenius2909@gmail.com>
2020-02-18 17:31:31 +0530

    answers added for all queries

---
+++ 14_16_react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Screenresizer from './components/Screenresizer'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Screenresizer /> 
+    </div>
+  );
+}
+
+export default App;
+
