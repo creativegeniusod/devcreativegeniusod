8edcc31dbf0aebdb9e0b5ccede1ca0e2799c2f95
creativegenius2909 <creativegenius2909@gmail.com>
2020-02-19 15:52:08 +0530

    add react test

--- 14_16_react/src/App.js
+++ 14_16_react/src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-
 import Screen from './Screen';
 
 class InputBox extends React.Component {
-    constructor(props){
-		super(props)
-        this.state={
-			height : this.props.height
-		}
-	}
-    render(){
-        const { onkeyPress, onChange, height } = this.props;
-		return(
-            <div>
-              <input
-                  type="text"
-                  value={height}
-                  onKeyPress={onkeyPress}
-                  onChange={onChange}
-              />
-            </div>
-		)
-	}
+  constructor(props){
+    super(props)
+    this.state={
+      height : this.props.height
+    }
+  }
+  
+  render(){
+    const { onkeyPress, onChange, height } = this.props;
+	return(
+      <div>
+        <input
+          type="text"
+          value={height}
+          onKeyPress={onkeyPress}
+          onChange={onChange}
+        />
+      </div>
+    )
+  }
 }
 const App = Screen(InputBox);
 
